17d5c636075c5fa9119b5e744b57713b3d080384
JP <85654561+jp524@users.noreply.github.com>
2022-08-04 11:31:45 -0400

    Refactor code into multiple classes

--- game.rb
+++ game.rb
@@ -1,25 +1,38 @@
-class RunGame
-  @colour_legend = { 1 => 'black',
-                     2 => 'white',
-                     3 => 'purple',
-                     4 => 'orange',
-                     5 => 'green',
-                     6 => 'yellow',
-                     7 => 'blue',
-                     8 => 'magenta' }
-  @colours = [1, 2, 3, 4, 5, 6, 7, 8]
-  @keys = %w[white red]
-  @secret_combination = []
+# class Codebreaker
 
-  def self.create_secret_combination
+# end
+
+class Codemaker
+  def initialize
+    @colours = [1, 2, 3, 4, 5, 6, 7, 8]
+    @secret_combination = []
+    @colour_legend = {  1 => 'black',
+                        2 => 'white',
+                        3 => 'purple',
+                        4 => 'orange',
+                        5 => 'green',
+                        6 => 'yellow',
+                        7 => 'blue',
+                        8 => 'magenta' }
+    generate_code
+  end
+
+  def generate_code
     4.times do
       @secret_combination.push(@colours.sample)
     end
   end
 
-  create_secret_combination
-
-  @secret_combination.each do |colour|
-    print "#{@colour_legend[colour]} "
+  def display_code
+    @secret_combination.each do |colour|
+      print "#{@colour_legend[colour]} "
+    end
   end
 end
+
+# class RunGame
+#   @keys = %w[white red]
+# end
+
+code = Codemaker.new
+code.display_code
